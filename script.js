c85b9738a415619e8a0c55f7a039d1b2a1889a5f
bhawana-web <bhawnav161102@gmail.com>
2024-12-03 20:52:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+document.querySelectorAll('.color-link').forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    const color = e.target.dataset.color;
+    const mainImage = document.querySelector('.photo-main img');
+    
+    // Update the main image based on the selected color
+    switch (color) {
+      case 'green':
+        mainImage.src = 'green-apple.jpg';
+        break;
+      case 'yellow':
+        mainImage.src = 'yellow-apple.jpg';
+        break;
+      case 'orange':
+        mainImage.src = 'orange-apple.jpg';
+        break;
+      case 'red':
+        mainImage.src = 'red-apple.jpg';
+        break;
+    }
+  });
+});
